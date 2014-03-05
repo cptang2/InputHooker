b6ddc9f87e4e9f5155b4a8dad4ccac149f069324
cptang2 <cptang92@gmail.com>
2014-03-05 12:04:55 -0800

    Record GUI Automation
    Stable release

---
+++ InputHooker/Record.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace InputHooker
+{
+    partial class RecordTC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (eventMes != null && eventMes.IsAlive)
+                eventMes.Abort();
+
+            if (file != null)
+                file.Close();
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mouseKeyEventProvider1 = new MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider();
+            this.MousePos = new System.Windows.Forms.TextBox();
+            this.ClickCount = new System.Windows.Forms.TextBox();
+            this.ScreenCoords = new System.Windows.Forms.TextBox();
+            this.Record = new System.Windows.Forms.Button();
+            this.StopRecord = new System.Windows.Forms.Button();
+            this.FileOpen = new System.Windows.Forms.TextBox();
+            this.SaveFile = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.MouseBut = new System.Windows.Forms.TextBox();
+            this.EventLog = new System.Windows.Forms.TextBox();
+            this.ChangeColor = new System.Windows.Forms.Button();
+            this.Detents = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mouseKeyEventProvider1
+            // 
+            this.mouseKeyEventProvider1.Enabled = false;
+            this.mouseKeyEventProvider1.HookType = MouseKeyboardActivityMonitor.Controls.HookType.Global;
+            this.mouseKeyEventProvider1.MouseDownExt += new System.EventHandler<MouseKeyboardActivityMonitor.MouseEventExtArgs>(this.mouseKeyEventProvider1_MouseDownExt);
+            this.mouseKeyEventProvider1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.mouseKeyEventProvider1_KeyUp);
+            this.mouseKeyEventProvider1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.mouseKeyEventProvider1_KeyDown);
+            this.mouseKeyEventProvider1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mouseKeyEventProvider1_MouseUp);
+            this.mouseKeyEventProvider1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mouseKeyEventProvider1_MouseDoubleClick);
+            this.mouseKeyEventProvider1.MouseWheel += new System.EventHandler<System.Windows.Forms.MouseEventArgs>(this.mouseKeyEventProvider1_MouseWheel);
+            this.mouseKeyEventProvider1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouseKeyEventProvider1_MouseMove);
+            // 
+            // MousePos
+            // 
+            this.MousePos.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MousePos.Location = new System.Drawing.Point(12, 99);
+            this.MousePos.Name = "MousePos";
+            this.MousePos.Size = new System.Drawing.Size(100, 20);
+            this.MousePos.TabIndex = 0;
+            // 
+            // ClickCount
+            // 
+            this.ClickCount.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ClickCount.Location = new System.Drawing.Point(12, 12);
+            this.ClickCount.Name = "ClickCount";
+            this.ClickCount.Size = new System.Drawing.Size(32, 20);
+            this.ClickCount.TabIndex = 1;
+            // 
+            // ScreenCoords
+            // 
+            this.ScreenCoords.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ScreenCoords.Location = new System.Drawing.Point(12, 125);
+            this.ScreenCoords.Name = "ScreenCoords";
+            this.ScreenCoords.Size = new System.Drawing.Size(100, 20);
+            this.ScreenCoords.TabIndex = 2;
+            // 
+            // Record
+            // 
+            this.Record.Location = new System.Drawing.Point(197, 93);
+            this.Record.Name = "Record";
+            this.Record.Size = new System.Drawing.Size(75, 23);
+            this.Record.TabIndex = 3;
+            this.Record.Text = "Record";
+            this.Record.UseVisualStyleBackColor = true;
+            this.Record.Click += new System.EventHandler(this.Record_Click);
+            // 
+            // StopRecord
+            // 
+            this.StopRecord.Location = new System.Drawing.Point(197, 122);
+            this.StopRecord.Name = "StopRecord";
+            this.StopRecord.Size = new System.Drawing.Size(75, 23);
+            this.StopRecord.TabIndex = 4;
+            this.StopRecord.Text = "Stop Record";
+            this.StopRecord.UseVisualStyleBackColor = true;
+            this.StopRecord.Click += new System.EventHandler(this.StopRecord_Click);
+            // 
+            // FileOpen
+            // 
+            this.FileOpen.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FileOpen.Location = new System.Drawing.Point(12, 54);
+            this.FileOpen.Name = "FileOpen";
+            this.FileOpen.Size = new System.Drawing.Size(228, 20);
+            this.FileOpen.TabIndex = 5;
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.Location = new System.Drawing.Point(246, 54);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(26, 20);
+            this.SaveFile.TabIndex = 6;
+            this.SaveFile.UseVisualStyleBackColor = true;
+            this.SaveFile.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // MouseBut
+            // 
+            this.MouseBut.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MouseBut.Location = new System.Drawing.Point(237, 12);
+            this.MouseBut.Name = "MouseBut";
+            this.MouseBut.Size = new System.Drawing.Size(34, 20);
+            this.MouseBut.TabIndex = 7;
+            // 
+            // EventLog
+            // 
+            this.EventLog.BackColor = System.Drawing.Color.White;
+            this.EventLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.EventLog.Cursor = System.Windows.Forms.Cursors.Default;
+            this.EventLog.Location = new System.Drawing.Point(13, 151);
+            this.EventLog.Name = "EventLog";
+            this.EventLog.Size = new System.Drawing.Size(259, 13);
+            this.EventLog.TabIndex = 8;
+            // 
+            // ChangeColor
+            // 
+            this.ChangeColor.Location = new System.Drawing.Point(1, -4);
+            this.ChangeColor.Name = "ChangeColor";
+            this.ChangeColor.Size = new System.Drawing.Size(277, 10);
+            this.ChangeColor.TabIndex = 10;
+            this.ChangeColor.Text = "Change Color";
+            this.ChangeColor.UseVisualStyleBackColor = true;
+            this.ChangeColor.Click += new System.EventHandler(this.ChangeColor_Click);
+            // 
+            // Detents
+            // 
+            this.Detents.Location = new System.Drawing.Point(197, 12);
+            this.Detents.Name = "Detents";
+            this.Detents.Size = new System.Drawing.Size(31, 20);
+            this.Detents.TabIndex = 11;
+            // 
+            // RecordTC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(284, 167);
+            this.Controls.Add(this.Detents);
+            this.Controls.Add(this.ChangeColor);
+            this.Controls.Add(this.EventLog);
+            this.Controls.Add(this.MouseBut);
+            this.Controls.Add(this.SaveFile);
+            this.Controls.Add(this.FileOpen);
+            this.Controls.Add(this.StopRecord);
+            this.Controls.Add(this.Record);
+            this.Controls.Add(this.ScreenCoords);
+            this.Controls.Add(this.ClickCount);
+            this.Controls.Add(this.MousePos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "RecordTC";
+            this.Text = "Record Test Case";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider mouseKeyEventProvider1;
+        private System.Windows.Forms.TextBox MousePos;
+        private System.Windows.Forms.TextBox ClickCount;
+        private System.Windows.Forms.TextBox ScreenCoords;
+        private System.Windows.Forms.Button Record;
+        private System.Windows.Forms.Button StopRecord;
+        private System.Windows.Forms.TextBox FileOpen;
+        private System.Windows.Forms.Button SaveFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox MouseBut;
+        private System.Windows.Forms.TextBox EventLog;
+        private System.Windows.Forms.Button ChangeColor;
+        private System.Windows.Forms.TextBox Detents;
+    }
+}
+
---
+++ InputHooker/Record.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+
+using MouseKeyboardActivityMonitor;
+// Assembly from http://globalmousekeyhook.codeplex.com/
+
+
+namespace InputHooker
+{
+    public partial class RecordTC : Form
+    {
+        private delegate void setMessage();
+        private delegate void clearMessage();
+        private setMessage setMDelegate;
+        private clearMessage clearMDelegate;
+
+        private bool blockInput = false;
+        private bool pause = false;
+        private int mouseDownCount = 0;
+        private int detents = 0;
+        private Thread eventMes;
+        string filePath;
+        string directoryPath;
+        string message;
+        string currentDir;
+        StreamWriter file;
+        
+        private Rectangle bounds = Screen.GetBounds(Point.Empty);
+        
+        public RecordTC()
+        {
+            InitializeComponent();
+            this.BackColor = randomColor();
+            ChangeColor.BackColor = this.BackColor;
+            ChangeColor.FlatStyle = FlatStyle.Flat;
+            ChangeColor.FlatAppearance.BorderSize = 0;
+
+            setMDelegate = new setMessage(setMess);
+            clearMDelegate = new clearMessage(clearMess);
+
+            MousePos.ReadOnly = true;
+            MousePos.BackColor = Color.Lavender;
+            ScreenCoords.ReadOnly = true;
+            ScreenCoords.BackColor = Color.Lavender;
+
+            ClickCount.ReadOnly = true;
+            ClickCount.BackColor = Color.MintCream;
+            MouseBut.ReadOnly = true;
+            MouseBut.BackColor = Color.MintCream;
+            FileOpen.ReadOnly = true;
+            FileOpen.BackColor = Color.MintCream;
+
+            EventLog.ReadOnly = true;
+            EventLog.BackColor = this.BackColor;
+
+            mouseKeyEventProvider1.Enabled = true;
+            ScreenCoords.Text = "(" + bounds.Left + "," + bounds.Top + "," + bounds.Right + "," + bounds.Bottom + ")";
+
+            saveFileDialog1.Filter = "CSV File |*.csv";
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.Title = "Save a CSV File";
+
+            currentDir = Environment.CurrentDirectory;
+        }
+
+        //Start event messages on another thread:
+        private void eventMessage()
+        {
+            EventLog.Invoke(setMDelegate);
+            Thread.Sleep(10000);
+
+            EventLog.Invoke(clearMDelegate);
+        }
+
+        private void setMess()
+        {
+            EventLog.Text = message;
+        }
+
+        private void clearMess()
+        {
+            EventLog.Clear();
+        }
+
+        //Start recording if a valid file was selected:
+        private void Record_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(directoryPath))
+            {
+                EventLog.Text = "Recording";
+                blockInput = true;
+                file = new StreamWriter(filePath);
+            }
+            else
+            {
+                message = "No test case selected";
+                eventMes = new Thread(new ThreadStart(eventMessage));
+                eventMes.Start();
+
+                while (!eventMes.IsAlive) ;
+            }
+        }
+
+
+        //Stop recording and clean up:
+        private void StopRecord_Click(object sender, EventArgs e)
+        {
+            if (blockInput)
+            {
+                EventLog.Clear();
+                blockInput = false;
+                mouseDownCount = 0;
+                file.Close();
+                SaveFile.Enabled = true;
+            }
+        }
+
+
+        //Display current mouse position in text box:
+        private void mouseKeyEventProvider1_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePos.Text = e.X + "," + e.Y;
+        }
+
+        //Take a snapshot before handling mousedown event:
+        private void mouseKeyEventProvider1_MouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            MouseBut.Text = "Down";
+            detents = 0;
+            Detents.Text = detents.ToString();
+            if (blockInput)
+            {
+                e.Handled = true;
+
+                mouseDownCount++;
+                ClickCount.Text = mouseDownCount.ToString();
+                //downInstance = true;
+                saveStep(directoryPath);
+
+                e.Handled = false;
+
+                file.WriteLine(e.Timestamp);
+                file.WriteLine("{0},{1},{2}", e.Button.ToString(), e.X, e.Y);
+            }
+        }
+
+        //Record mouse up event:
+        private void mouseKeyEventProvider1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseBut.Text = "Up";
+
+            if (blockInput)
+                file.WriteLine("up{0},{1},{2}", e.Button, e.X, e.Y);
+        }
+
+        //Record doubleclick event:
+        private void mouseKeyEventProvider1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (blockInput)
+                file.WriteLine("doubleclick{0},{1},{2}", e.Button, e.X, e.Y);
+        }
+
+        //Save current screen:
+        private void saveStep(string path)
+        {
+            //Save the full screen:
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+
+                int post = 0;
+                while (File.Exists(Path.Combine(path, post + ".png")))
+                {
+                    post++;
+                }
+
+                bitmap.Save(Path.Combine(path, post + ".png"));
+                file.WriteLine("image,{0}", post + ".png");
+            }
+        }
+
+        //Open the save file dialog:
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+
+            filePath = saveFileDialog1.FileName;
+
+            if (filePath.Length > 0)
+            {
+                directoryPath = Path.Combine(Path.GetDirectoryName(filePath),
+                                Path.GetFileNameWithoutExtension(saveFileDialog1.FileName));
+
+                if (!File.Exists(filePath) && !Directory.Exists(directoryPath))
+                {
+                    //File.Create(saveFileDialog1.FileName);
+                    Directory.CreateDirectory(directoryPath);
+                    ClickCount.Text = "0";
+                    File.Copy(Path.Combine(currentDir, "blankConfig.xml"),
+                                Path.Combine(directoryPath, "config.xml"));
+                    FileOpen.Text = Path.GetFileName(saveFileDialog1.FileName);
+                    SaveFile.Enabled = false;
+                }
+            }
+        }
+
+        //Record Key up:
+        private void mouseKeyEventProvider1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (blockInput)
+                file.WriteLine("keyup,{0}", (int)e.KeyCode);
+        }
+
+        //Change GUI color randomly:
+        private Color randomColor()
+        {
+            Random random = new Random();
+            return Color.FromArgb(255, random.Next(1, 255), random.Next(1, 255), random.Next(1, 255));
+        }
+
+        private void ChangeColor_Click(object sender, EventArgs e)
+        {
+            this.BackColor = randomColor();
+            EventLog.BackColor = this.BackColor;
+            ChangeColor.BackColor = this.BackColor;
+        }
+
+        private void mouseKeyEventProvider1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((int)e.KeyCode == 44)
+                e.Handled = true;
+
+            if (blockInput && (int)e.KeyCode == 44)
+            {
+                EventLog.Text = "Paused";
+                pause = true;
+                blockInput = false;
+                Record.Enabled = false;
+                StopRecord.Enabled = false;
+            }
+            else if (pause && !blockInput && (int)e.KeyCode == 44)
+            {
+                EventLog.Text = "Recording";
+                blockInput = true;
+                pause = false;
+                Record.Enabled = true;
+                StopRecord.Enabled = true;
+            }
+
+            if (blockInput)
+                file.WriteLine("keydown,{0}", (int)e.KeyCode);
+        }
+
+        private void mouseKeyEventProvider1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            detents += e.Delta;
+            Detents.Text = detents.ToString();
+            if (blockInput)
+            {
+                file.WriteLine("detent,{0},{1},{2}", e.X, e.Y, e.Delta);
+            }
+        }
+    }
+}
